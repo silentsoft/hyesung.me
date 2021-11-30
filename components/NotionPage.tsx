b79ac8240b018c416e2021402e0277506ee25433
silentsoft <hyesung.lee@silentsoft.org>
2021-11-30 22:40:40 +0900

    Hide page aside

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -19,7 +19,7 @@ import { getBlockTitle } from 'notion-utils'
 import { mapPageUrl, getCanonicalPageUrl } from 'lib/map-page-url'
 import { mapNotionImageUrl } from 'lib/map-image-url'
 import { getPageDescription } from 'lib/get-page-description'
-import { getPageTweet } from 'lib/get-page-tweet'
+// import { getPageTweet } from 'lib/get-page-tweet'
 import { searchNotion } from 'lib/search-notion'
 import * as types from 'lib/types'
 import * as config from 'lib/config'
@@ -29,9 +29,9 @@ import { CustomFont } from './CustomFont'
 import { Loading } from './Loading'
 import { Page404 } from './Page404'
 import { PageHead } from './PageHead'
-import { PageActions } from './PageActions'
+// import { PageActions } from './PageActions'
 import { Footer } from './Footer'
-import { PageSocial } from './PageSocial'
+// import { PageSocial } from './PageSocial'
 // import { GitHubShareButton } from './GitHubShareButton'
 import { ReactUtterances } from './ReactUtterances'
 
@@ -140,7 +140,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
     getPageDescription(block, recordMap) ?? config.description
 
   let comments: React.ReactNode = null
-  let pageAside: React.ReactChild = null
+  // let pageAside: React.ReactChild = null
 
   // only display comments and page actions on blog post pages
   if (isBlogPost) {
@@ -155,12 +155,12 @@ export const NotionPage: React.FC<types.PageProps> = ({
       )
     }
 
-    const tweet = getPageTweet(block, recordMap)
-    if (tweet) {
-      pageAside = <PageActions tweet={tweet} />
-    }
+    // const tweet = getPageTweet(block, recordMap)
+    // if (tweet) {
+    //   pageAside = <PageActions tweet={tweet} />
+    // }
   } else {
-    pageAside = <PageSocial />
+    // pageAside = <PageSocial />
   }
 
   return (
@@ -271,7 +271,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         mapImageUrl={mapNotionImageUrl}
         searchNotion={searchNotion}
         pageFooter={comments}
-        pageAside={pageAside}
+        // pageAside={pageAside}
         footer={
           <Footer
             isDarkMode={darkMode.value}
