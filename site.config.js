4be43fc89fb7e8684b37f0568ee329474402a4e2
silentsoft <hyesung.lee@silentsoft.org>
2021-12-07 16:24:15 +0900

    Override page url for '/details'

--- site.config.js
+++ site.config.js
@@ -49,5 +49,7 @@ module.exports = {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null
+  pageUrlOverrides: {
+    '/details': '9b6cd4cad33d4c6ba49b078c38d998f2'
+  }
 }
